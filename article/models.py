12afd0464d25738c324943c370686c7dc6927978
luog11 <luog11@localhost.localdomain>
2016-08-09 20:52:12 +0800

    add commont box and not work now

--- article/models.py
+++ article/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Article(models.Model):
-	title = models.CharField(verbose_name='Title', max_length=100)
-	category = models.CharField(verbose_name='Category', max_length=50, blank=True)
-	date_time = models.DateField(verbose_name='Creation Date', auto_now_add=True)
-	content = models.TextField(verbose_name='Content', blank=True, null=True)
+    title = models.CharField(verbose_name='Title', max_length=100)
+    category = models.CharField(
+        verbose_name='Category', max_length=50, blank=True)
+    date_time = models.DateField(verbose_name='Creation Date', auto_now_add=True)
+    content = models.TextField(verbose_name='Content', blank=True, null=True)
 
-	def __unicode__(self):
-		return self.title
+    def get_absolute_url(self):
+        path = reverse('article', kwargs={'id':self.id})
+        return "http://127.0.0.1:8000%s" % path
 
-	class Meta:
-		ordering=['-date_time']
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering=['-date_time']
--- article/views.py
+++ article/views.py
@@ -7,8 +7,9 @@ def articles(request):
 	return render_to_response('article_list.html', {'post_list': post_list})
 
 def article(request, id):
+	
 	try:
-		post = Article.objects.get(id=str(id))
+		post = Article.objects.get(id=id)
 	except Article.DoesNotExist:
 		raise Http404
 	return render_to_response('article.html', {'post': post})
