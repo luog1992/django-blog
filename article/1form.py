8f97b15031d1f55862d88c02ece8071f92987482
luog11 <luog11@localhost.localdomain>
2016-08-19 21:21:17 +0800

    stuey ckeditor

--- article/1form.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django_bootstrap_markdown.widgets import MarkdownInput
-
-class PostForm(forms.Form):
-    title = forms.CharField()
-    markdown = forms.CharField(widget=MarkdownInput)
