b5219c98be65074e057783ad46d489bdcac92e3d
luog11 <luog1992@gmail.com>
2016-08-25 21:04:52 +0800

    nothing

--- article/admin.py
+++ article/admin.py
@@ -3,25 +3,25 @@ from article.models import Article, Tag, Category
 
 
 class ArticleAdmin(admin.ModelAdmin):
-	list_display  = ('title', 'date_time', 'summary')
-	search_fields = ('title', 'tags')
-	list_filter   = ('date_time', 'tags')
-	ordering      = ('-date_time', 'title')
-	fields        = ('title', 'tags', 'summary', 'content')
+    list_display = ('title', 'date_time', 'summary')
+    search_fields = ('title', 'tags')
+    list_filter = ('date_time', 'tags')
+    ordering = ('-date_time', 'title')
+    fields = ('title', 'tags', 'summary', 'content')
 
 
 class TagAdmin(admin.ModelAdmin):
-	list_display = ('name', 'color')
-	search_fields = ('name', 'color')
-	ordering = ('name', 'color')
-	fields = ('name', 'color')
+    list_display = ('name', 'color')
+    search_fields = ('name', 'color')
+    ordering = ('name', 'color')
+    fields = ('name', 'color')
 
 
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ('name', 'color')
-	search_fields = ('name', 'color')
-	ordering = ('name', 'color')
-	fields = ('name', 'color')
+    list_display = ('name', 'color')
+    search_fields = ('name', 'color')
+    ordering = ('name', 'color')
+    fields = ('name', 'color')
 
 
 # Register your models here
--- article/models.py
+++ article/models.py
@@ -4,7 +4,8 @@ from django.core.urlresolvers import reverse
 
 class Tag(models.Model):
     name = models.CharField(verbose_name='Tag', max_length=20)
-    color = models.CharField(verbose_name='Color', max_length=20, default='transparent')
+    color = models.CharField(verbose_name='Color',
+                             max_length=20, default='transparent')
 
     def __unicode__(self):
         return self.name
@@ -16,8 +17,10 @@ class Tag(models.Model):
 class Article(models.Model):
     title = models.CharField(verbose_name='Title', max_length=100)
     tags = models.ManyToManyField(Tag)
-    date_time = models.DateField(verbose_name='Creation Date', auto_now_add=True)
-    summary = models.TextField(verbose_name='Summary', max_length=1000, blank=True, null=True)
+    date_time = models.DateField(
+        verbose_name='Creation Date', auto_now_add=True)
+    summary = models.TextField(
+        verbose_name='Summary', max_length=1000, blank=True, null=True)
     content = models.TextField(verbose_name='Content', blank=True, null=True)
 
     def get_absolute_url(self):
@@ -33,7 +36,8 @@ class Article(models.Model):
 
 class Category(models.Model):
     name = models.CharField(verbose_name='Category', max_length=20)
-    color = models.CharField(verbose_name='Color', max_length=20,  default='transparent')
+    color = models.CharField(verbose_name='Color',
+                             max_length=20,  default='transparent')
     articles = models.ManyToManyField(Article)
 
     def __unicode__(self):
