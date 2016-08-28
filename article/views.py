fae9bf13985d60d73f0ed9911f1014298f0e692c
luog11 <luog1992@gmail.com>
2016-08-28 18:52:34 +0800

    add blog editor

--- article/views.py
+++ article/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template.context_processors import csrf
 from django.http import Http404
 from article.models import Blog, Tag, Category
 
@@ -7,10 +8,16 @@ from article.models import Blog, Tag, Category
 # 	post_list = Blog.objects.all()
 # 	return render_to_response('home.html', {'post_list': post_list})
 
+def search(request):
+    if 'q' in request.GET:
+        print request.GET['q']
+
+
 def blogs(request):
     blogs = Blog.objects.all()
     tag_cloud = Tag.objects.all().order_by('color')
     return render_to_response('blog_list.html', {'blogs': blogs, 'tag_cloud': tag_cloud})
+
 
 def blog_detail(request, id=0):
     id = int(id)
@@ -21,12 +28,47 @@ def blog_detail(request, id=0):
     tag_cloud = Tag.objects.all().order_by('color')
     return render_to_response('blog_detail.html', {'blog': blog, 'tag_cloud': tag_cloud})
 
+
+def blog_edit(request, id=0):
+    cxt = {}
+    cxt.update(csrf(request))
+    print '....Request.Method:', request.method
+    if request.method == 'POST':
+        print '....', request.POST.get('title')
+        print '....', request.POST.get('tags')
+        print '....', request.POST.get('content')
+        
+    id = int(id)
+    try:
+        blog = Blog.objects.get(id=id)
+    except Blog.DoesNotExist:
+        raise Http404
+
+    tag_cloud = Tag.objects.all().order_by('color')
+    cxt.update({'blog': blog, 'tag_cloud': tag_cloud})
+    return render_to_response('blog_edit.html', cxt)
+
+
 def tag_blogs(request, name):
-	try:
-		tag = Tag.objects.filter(name=name)[0]
-	except:
-		raise Http404
-	print tag
-	blogs = tag.blogs.all()
-	tag_cloud = Tag.objects.all().order_by('color')
-	return render_to_response('blog_list.html', {'blogs': blogs, 'tag_cloud': tag_cloud})
+    try:
+        tag = Tag.objects.filter(name=name)[0]
+    except:
+        raise Http404
+    print tag
+    blogs = tag.blogs.all()
+    tag_cloud = Tag.objects.all().order_by('color')
+    return render_to_response('blog_list.html', {'blogs': blogs, 'tag_cloud': tag_cloud})
+
+
+def request(request):
+    print '.... request.GET', request.GET
+    print '.... request.POST', request.POST
+    print '....', request.path
+    print '....', request.get_host()
+    print '....', request.get_full_path()
+    print '....', request.is_secure()
+    print '....', 'request.META'
+    print '........ HTTP_USER_AGENT', request.META['HTTP_USER_AGENT']
+    print '........ REMOTE_ADDR', request.META['REMOTE_ADDR']
+    # for key in request.META:
+    # print '........', key, request.META[key]
--- myblog/urls.py
+++ myblog/urls.py
@@ -13,8 +13,11 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('article.views',
+	url(r'^request/$', 'request'),
+	url(r'^search/$', 'search', name='search'),
 	url(r'^home/$', 'blogs', name='home'),
 	url(r'^blogs/$', 'blogs', name='blogs'),
-	url(r'^blog/(?P<id>\d{1,3})/$', 'blog_detail', name='blog_detail'),
+	url(r'^blog/(?P<id>\d{1,3})/detail$', 'blog_detail', name='blog_detail'),
+	url(r'^blog/(?P<id>\d{1,3})/edit/$', 'blog_edit', name='blog_edit'),
 	url(r'^tag/(?P<name>\w{1,20})', 'tag_blogs', name='tag_blogs'),
 )
